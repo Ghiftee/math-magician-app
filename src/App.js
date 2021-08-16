04e238de4112f4bdd22b55eb87b5bd96d2c17e75
Ghiftee <gigibetine@gmail.com>
2021-08-16 17:43:51 +0100

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import './App.css';
-import Calculator from './components/Calculator';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './pages/home';
+import CalcPage from './pages/calculator';
+import Quote from './pages/quotes';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <Router>
+        <header
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '20px',
+          }}
+        >
+          <h1>Math Magicians</h1>
+          <nav>
+            <Link className="p-10 border-right" to="/">Home</Link>
+            <Link className="p-10 border-right" to="/calculator">Calculator</Link>
+            <Link className="p-10" to="/quote">Quote</Link>
+          </nav>
+        </header>
+        <main
+          style={{
+            padding: '0px 20px',
+          }}
+        >
+          <Switch>
+            <Route path="/quote">
+              <Quote />
+            </Route>
+            <Route path="/calculator">
+              <CalcPage />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
