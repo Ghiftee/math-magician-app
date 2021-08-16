2150bd2d537c43382ef67a115c8a2bf8a4f3b6bf
Ghiftee <gigibetine@gmail.com>
2021-08-16 17:40:35 +0100

    Create quotes page

---
+++ src/pages/quotes.jsx
@@ -0,0 +1,16 @@
+export default function Quote() {
+  return (
+    <div>
+      <p
+        style={{
+          fontSize: '22px',
+          padding: '30px',
+          marginTop: '50px',
+        }}
+      >
+        Mathematics is not about numbers, equations, computations, or algorithms:
+        It is about understanding. -- William Paul Thurston
+      </p>
+    </div>
+  );
+}
